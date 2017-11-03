78ea3b74cbb17c8747967bbfe1dcbbb0c7593c48
Ara <ayaghsizian@gmail.com>
2017-11-03 00:35:23 +0000

    sumFibs works

--- sumFibs.c
+++ sumFibs.c
@@ -2,9 +2,13 @@
 #include <cs50.h>
 #include <math.h>
 
-int main (int n)
+int sumFibs (int num);
+
+int main (void)
 {
-    printf("This is the sum of all fibs %i: \n", n);
+    printf("we are going to add all the fibonacci numbers up to __: ");
+    int n = get_int();
+    printf("This is the sum of all fibs: %i \n", sumFibs(n) );
     return sumFibs(n);
 }
 
@@ -13,13 +17,13 @@ int sumFibs (int num)
     int sum = 1, priorfib = 1, curfib = 1;
     do
     {
-        if( curfib % 2 != 1 )
+        if( curfib % 2 != 0 )
         {
             sum += curfib;
         }
         int temp = priorfib;
         priorfib = curfib;
-        curfib = temp;
+        curfib = temp + priorfib;
     } while( curfib < num );
     return sum;
 }
