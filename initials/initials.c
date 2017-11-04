e2835859959f75894e1942febb4fd1970e7f5b1f
Ara <ayaghsizian@gmail.com>
2017-11-04 16:28:05 +0000

    finally used isaplha instead of trying to check for equality

--- initials/initials.c
+++ initials/initials.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <cs50.h>
-
+#include <string.h>
+#include <ctype.h>
 
 int main (void){
 
-    printf("how much due? ");
+    printf("Please type your name: ___ ");
 
+    string name = get_string();
+    int len = strlen(name);
+
+    printf("This is your name: %s\n", name);
+    printf("this is len: %d \n", len);
+
+    for (int i = 0; i < len; i++)
+    {
+        if(i == 0){
+            printf("%c", toupper(name[i]));
+        }
+
+        else if( !isalpha(name[i]) ){
+            printf("%c", toupper(name[i + 1]) );
+        }
+
+    }
+    printf("\n");
 }
